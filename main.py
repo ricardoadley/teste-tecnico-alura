278ee0424770b209ed59285c13f8e32d4fc1b004
ricardoadley <ricardo.sena@ccc.ufcg.edu.br>
2024-06-02 01:09:04 -0300

    feat: criação da rota para geração de pagina de feedback geral

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import logging
 import sys
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, make_response, render_template, request
 from flask_mail import Mail
 from flask_restx import Api, Resource, fields
 from sqlalchemy_utils import create_database, database_exists
@@ -34,6 +34,18 @@ class FeedbackList(Resource):
     @api.expect(feedback_model)
     def post(self):
         return feedback_controller.catch_feedback(request)
+
+@api.route('/report')
+class Report(Resource):
+    def get(self):
+        """
+        Renderiza o template HTML para o relatório de feedback.
+        Retorna o HTML do relatório.
+        """
+        feedbacks, positive_percentage, sorted_features = feedback_controller.generate_all_feedbacks_report()
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('report.html', feedbacks=feedbacks, positive_percentage=positive_percentage, sorted_features=sorted_features),200,headers)
+
 if __name__ == "__main__":
     #logging.info(f"Application started at port {5000}")
     #app.run(debug= '--debug' in sys.argv, port = PORT, host= DEPLOY_URL)
